0f8d76d09ce88c3f73d0d8e14191d3dd2036b1d4
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-04-16 22:32:32 +0200

    last changes
    server

--- server.js
+++ server.js
@@ -9,8 +9,27 @@ const apiUrl = 'https://redpers.nl/wp-json/wp/v2';
 
 // const data_c = await fetchJson('https://fdnd.directus.app/items/person/40')
 
+// trying out a live server
+// var liveServer = require("live-server");
+
+// var liveServer = require("live-server");
+
+// var params = {
+// 	port: 8008, // Set the server port. Defaults to 8080.
+// 	host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+// 	root: "./views", // Set root directory that's being served. Defaults to cwd.
 // 	open: false, // When false, it won't load your browser by default.
 // 	ignore: '', // comma-separated string for paths to ignore
+// 	file: "index.ejs", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+// 	wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+// 	mount: [['/components', './node_modules']], // Mount a directory to a route.
+// 	logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+// 	middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+// };
+
+// liveServer.start(params);
+
+
 // Maak een nieuwe express app aan
 const app = express()
 
@@ -81,6 +100,7 @@ app.get('/author/:id',function(req,res){
   });
 
 })
+
 
 // TODO:
 // Category endpoint: app.get('/category/:categoryID'....)
